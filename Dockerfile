8224744bb50ecce0d8b007344bd8ef8a894540d6
Sundy.Lee.mac <656233622@qq.com>
2020-06-11 15:24:11 +0800

    1.链接mysql数据库 2.修改django为3.0.7

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM python:3.7
 WORKDIR /home
 COPY . /home
-RUN pip install -i https://pypi.tuna.tsinghua.edu.cn/simple  -r requirements.txt && \
-    python manage.py makemigrations && python manage.py migrate
+RUN pip install -i http://mirrors.aliyun.com/pypi/simple/  -r requirements.txt
 EXPOSE 8383
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8383"]
--- online_judge_server/settings.py
+++ online_judge_server/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'f2how(cqd74k53xclxf5g)zxjow(925!6vylcoxn50v=06w!a-'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -73,17 +71,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'online_judge_server.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'online_judge_server_database',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -102,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
