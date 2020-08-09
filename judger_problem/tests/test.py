328dd5395cb19f5929d1f0eef6ef4f30229c500e
Sundy.Lee.mac <656233622@qq.com>
2020-08-09 17:19:42 +0800

    1.修复bug 题目正确后增加正确题目列表

--- judger_problem/tests/test.py
+++ judger_problem/tests/test.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from judger_problem.models import Problem
 from django.contrib.auth.models import User
+from account.models import UserInfo
 from django.urls import reverse
 import pdb
 
@@ -9,13 +10,19 @@ import pdb
 
 class ProblemTest(TestCase):
     def save_auth(self):
-        user = User()
-        user.username = "sundy"
-        user.password = "123"
+        """
+        创建一个用户
+        :return:
+        """
+        user = User.objects.create_user("sundy", "sundy@qq.com", "123")
         user.save()
+        UserInfo(user=user).save()
 
     def save_problem(self):
-        self.save_auth()
+        """
+        创建一个题目
+        :return:
+        """
         problem = Problem()
         problem.title = "入门测试题目"
         problem.problem_content = "xxxxx"
@@ -27,15 +34,39 @@ class ProblemTest(TestCase):
         problem.save()
 
     def test_save_problem(self):
+        """
+        测试保存题目功能
+        :return:
+        """
+        self.save_auth()
         self.save_problem()
         self.assertEqual(Problem.objects.get().title, "入门测试题目", "题目保存失败")
 
     def login(self):
-        self.client.post(reverse('account:login'), {'user_name': 'sundy', 'user_password1': '123'}, follow=True)
+        """
+        登陆
+        :return:
+        """
+        self.client.post(reverse('account:login'),
+                         {'user_name': 'sundy', 'user_password1': '123'},
+                         follow=True)
 
-    # def test_submit_code(self):
-    #     self.login()
-    #     file = open("judger_problem/tests/a_b.cpp", 'r')
-    #     response = self.client.post(reverse('problem:problemdetail', args=[1]), {'user_code': file.read()})
-    #     pdb.set_trace()
-    #     print("")
+    def test_submit_code(self):
+        """
+        测试提交代码
+        :return:
+        """
+        self.save_auth()
+        self.save_problem()
+        self.login()
+        self.assertEqual(Problem.objects.first().Submits, 0, "代码提交总数初始值应该为0")
+        file = open("judger_problem/tests/a_b.cpp", 'r')
+        response = self.client.post(
+            reverse('problem:problemdetail', args=[Problem.objects.first().id]),
+            {'user_code': file.read()}, follow=True)
+        self.assertEqual(Problem.objects.first().Submits, 1, "提交代码时候，代码提交总数没变")
+        self.assertEqual(
+            len(User.objects.first().user_info.right_problems.all()), 1,
+            "用户信息表中正确题目没变")
+
+        print(response)
